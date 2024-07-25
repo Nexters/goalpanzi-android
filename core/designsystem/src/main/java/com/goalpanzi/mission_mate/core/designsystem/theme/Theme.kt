7fd1f8243c5322a57e3b3b9d6e13e886c0206fe3
Jung SeokJoon <eshc.joon@gmail.com>
2024-07-25 21:49:18 +0900

    fix Typography

--- core/designsystem/src/main/java/com/goalpanzi/mission_mate/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/goalpanzi/mission_mate/core/designsystem/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.goalpanzi.mission_mate.core.designsystem.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -21,16 +20,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -52,7 +41,6 @@ fun MissionmateTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
