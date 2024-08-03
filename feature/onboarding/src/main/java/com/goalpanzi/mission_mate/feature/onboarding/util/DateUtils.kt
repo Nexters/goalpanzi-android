90cd3464a0f0a072ac1ac3f69b56749d53c77082
Jung SeokJoon <eshc.joon@gmail.com>
2024-08-03 20:47:40 +0900

    add DateUtils

---
+++ feature/onboarding/src/main/java/com/goalpanzi/mission_mate/feature/onboarding/util/DateUtils.kt
@@ -0,0 +1,46 @@
+package com.goalpanzi.mission_mate.feature.onboarding.util
+
+import java.time.DayOfWeek
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+object DateUtils {
+    private fun convertMillisToLocalDateWithFormatter(date: LocalDate, dateTimeFormatter: DateTimeFormatter) : LocalDate {
+        val dateInMillis = LocalDate.parse(date.format(dateTimeFormatter), dateTimeFormatter)
+            .atStartOfDay(ZoneId.systemDefault())
+            .toInstant()
+            .toEpochMilli()
+
+        return Instant
+            .ofEpochMilli(dateInMillis)
+            .atZone(ZoneId.systemDefault())
+            .toLocalDate()
+    }
+    fun dateToString(date: LocalDate): String {
+        val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd", Locale.getDefault())
+        val dateInMillis = convertMillisToLocalDateWithFormatter(date, dateFormatter)
+        return dateFormatter.format(dateInMillis)
+    }
+
+    fun longToLocalDate(milliseconds: Long): LocalDate {
+        val instant = Instant.ofEpochMilli(milliseconds)
+
+        return instant.atZone(ZoneId.systemDefault()).toLocalDate()
+    }
+
+    fun localDateToMillis(localDate: LocalDate?): Long? {
+        val instant = localDate?.atStartOfDay(ZoneId.of("UTC"))?.toInstant() ?: return null
+
+        return instant.toEpochMilli()
+    }
+
+    fun filterDatesByDayOfWeek(startDate: LocalDate?, endDate: LocalDate?, days: List<DayOfWeek>): Int {
+        if(startDate == null || endDate == null) return 0
+        return generateSequence(startDate) { date ->
+            if (date.isBefore(endDate)) date.plusDays(1) else null
+        }.filter { it.dayOfWeek in days }.toList().size
+    }
+}
