c52ea9a4c48c1454c0f66f2c03b72e2e424af8b2
eshc123 <>
2024-08-05 22:36:58 +0900

    add MissionUiModel & CodeResultEvent

---
+++ feature/onboarding/src/main/java/com/goalpanzi/mission_mate/feature/onboarding/model/MissionUiModel.kt
@@ -0,0 +1,8 @@
+package com.goalpanzi.mission_mate.feature.onboarding.model
+
+data class MissionUiModel(
+    val missionTitle : String,
+    val missionPeriod : String,
+    val missionDays : String,
+    val missionTime : String
+)
