d04d73d8089d8e652f641980d107c27c0909edf0
eshc123 <>
2024-08-06 00:25:40 +0900

    mod UI 수정 - 문구 및 버튼

--- feature/onboarding/src/main/java/com/goalpanzi/mission_mate/feature/onboarding/screen/boardsetup/BoardSetupSuccessScreen.kt
+++ feature/onboarding/src/main/java/com/goalpanzi/mission_mate/feature/onboarding/screen/boardsetup/BoardSetupSuccessScreen.kt
@@ -1,10 +1,13 @@
 package com.goalpanzi.mission_mate.feature.onboarding.screen.boardsetup
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -13,8 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.goalpanzi.mission_mate.core.designsystem.component.MissionMateButtonType
 import com.goalpanzi.mission_mate.core.designsystem.component.MissionMateTextButton
@@ -31,49 +36,84 @@ fun BoardSetupSuccessScreen(
     onClickStart: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(ColorWhite_FFFFFFFF)
-            .padding(horizontal = 14.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+    Box(
+        modifier = modifier.background(ColorWhite_FFFFFFFF)
     ) {
-        Text(
-            modifier = Modifier.statusBarsPadding().padding(top = 60.dp),
-            text = stringResource(id = R.string.onboarding_board_setup_success_title),
-            style = MissionMateTypography.heading_sm_bold,
-            color = ColorGray2_FF4F505C
-        )
-
-        Text(
-            text = stringResource(id = R.string.onboarding_board_setup_success_description),
-            style = MissionMateTypography.title_xl_regular,
-            color = ColorGray3_FF727484,
-            textAlign = TextAlign.Center
-        )
-        Box(
+        Image(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            contentAlignment = Alignment.Center
-        ) {
-            StableImage(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                drawableResId = com.goalpanzi.mission_mate.core.designsystem.R.drawable.image_jeju_success,
-                contentScale = ContentScale.FillWidth
-            )
-        }
-        MissionMateTextButton(
-            modifier = Modifier
-                .padding(vertical = 36.dp, horizontal = 10.dp)
                 .fillMaxWidth()
                 .wrapContentHeight(),
-            buttonType = MissionMateButtonType.ACTIVE,
-            textId = R.string.start,
-            onClick = onClickStart
+            painter = painterResource(id = com.goalpanzi.mission_mate.core.designsystem.R.drawable.background_jeju),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
         )
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+                .padding(horizontal = 14.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .padding(top = 48.dp),
+                text = stringResource(id = R.string.onboarding_board_setup_success_title),
+                style = MissionMateTypography.heading_sm_bold,
+                color = ColorGray1_FF404249
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                StableImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    drawableResId = com.goalpanzi.mission_mate.core.designsystem.R.drawable.image_jeju_success,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.onboarding_board_setup_success_description_top),
+                    style = MissionMateTypography.title_xl_bold,
+                    color = ColorGray3_FF727484,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = stringResource(id = R.string.onboarding_board_setup_success_description_bottom),
+                    style = MissionMateTypography.title_xl_regular,
+                    color = ColorGray3_FF727484,
+                    textAlign = TextAlign.Center
+                )
+            }
+            MissionMateTextButton(
+                modifier = Modifier
+                    .padding(bottom = 36.dp, start = 10.dp, end = 10.dp, top = 71.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                buttonType = MissionMateButtonType.ACTIVE,
+                textId = R.string.start,
+                onClick = onClickStart
+            )
+        }
     }
+
+}
+
+@Preview
+@Composable
+fun PreviewBoardSetupSuccessScreen(){
+    BoardSetupSuccessScreen(
+        onClickStart = {},
+        modifier = Modifier.fillMaxSize()
+    )
 }
