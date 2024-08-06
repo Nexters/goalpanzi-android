e07f670c42a8ffb68a951e40a079a07a0da1c38d
eshc123 <>
2024-08-06 21:11:00 +0900

    chore 주석 제거

--- core/network/src/main/java/com/goalpanzi/mission_mate/core/network/service/OnboardingService.kt
+++ core/network/src/main/java/com/goalpanzi/mission_mate/core/network/service/OnboardingService.kt
@@ -6,12 +6,6 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-/*
-* 온보딩 화면에서 사용되는 API
-*  - 미션 생성
-*  - 미션 조회 by 초대코드
-*  - 미션 참여
-*/
 interface OnboardingService {
     @POST("/api/missions")
     suspend fun createMission(
