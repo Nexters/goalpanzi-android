11edcdb81a7e3bef051be97fcf93b06cdfe50f60
Changyeop Lee <paulus0617@gmail.com>
2024-08-13 02:06:16 +0900

    Remove SSL code

--- core/network/src/main/java/com/goalpanzi/mission_mate/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/goalpanzi/mission_mate/core/network/di/NetworkModule.kt
@@ -7,18 +7,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
-import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import java.security.KeyStore
 import javax.inject.Singleton
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -36,16 +31,9 @@ internal object NetworkModule {
         httpLoggingInterceptor: HttpLoggingInterceptor,
         tokenInterceptor: TokenInterceptor
     ): OkHttpClient {
-        // TLS 대응
-        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        trustManagerFactory.init(null as KeyStore?)
-        val trustManager = trustManagerFactory.trustManagers[0] as X509TrustManager
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, arrayOf(trustManager), java.security.SecureRandom())
 
         return OkHttpClient.Builder()
             .addInterceptor(tokenInterceptor)
-            .sslSocketFactory(sslContext.socketFactory, trustManager)
             .addInterceptor(httpLoggingInterceptor)
             .build()
     }
