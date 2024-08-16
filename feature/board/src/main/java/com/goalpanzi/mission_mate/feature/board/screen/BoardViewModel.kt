b1cadbd5576c80c3347fe53178d9cd9040f6546e
eshc123 <>
2024-08-16 23:16:17 +0900

    mod missionState, boardRewardEvent 로직 추가

--- feature/board/src/main/java/com/goalpanzi/mission_mate/feature/board/screen/BoardViewModel.kt
+++ feature/board/src/main/java/com/goalpanzi/mission_mate/feature/board/screen/BoardViewModel.kt
@@ -7,18 +7,28 @@ import com.goalpanzi.mission_mate.core.domain.usecase.DeleteMissionUseCase
 import com.goalpanzi.mission_mate.core.domain.usecase.GetMissionBoardsUseCase
 import com.goalpanzi.mission_mate.core.domain.usecase.GetMissionUseCase
 import com.goalpanzi.mission_mate.core.domain.usecase.GetMissionVerificationsUseCase
-import com.goalpanzi.mission_mate.feature.board.model.MissionBoardUiModel
-import com.goalpanzi.mission_mate.feature.board.model.MissionUiModel
-import com.goalpanzi.mission_mate.feature.board.model.MissionVerificationUiModel
+import com.goalpanzi.mission_mate.feature.board.model.MissionState
+import com.goalpanzi.mission_mate.feature.board.model.MissionState.Companion.getMissionState
+import com.goalpanzi.mission_mate.feature.board.model.uimodel.MissionBoardUiModel
+import com.goalpanzi.mission_mate.feature.board.model.uimodel.MissionUiModel
+import com.goalpanzi.mission_mate.feature.board.model.uimodel.MissionVerificationUiModel
 import com.luckyoct.core.model.base.NetworkResult
+import com.luckyoct.core.model.response.BoardReward
+import com.luckyoct.core.model.response.MissionBoardsResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -31,8 +41,8 @@ class BoardViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val missionId : Long = savedStateHandle.get<Long>("missionId")!!
-    
+    private val missionId: Long = savedStateHandle.get<Long>("missionId")!!
+
     private val _deleteMissionResultEvent = MutableSharedFlow<Boolean>()
     val deleteMissionResultEvent: SharedFlow<Boolean> = _deleteMissionResultEvent.asSharedFlow()
 
@@ -49,7 +59,38 @@ class BoardViewModel @Inject constructor(
     val missionVerificationUiModel: StateFlow<MissionVerificationUiModel> =
         _missionVerificationUiModel.asStateFlow()
 
+    val missionState: StateFlow<MissionState> =
+        combine(
+            missionBoardUiModel.filter { it is MissionBoardUiModel.Success },
+            missionUiModel.filter { it is MissionUiModel.Success },
+            missionVerificationUiModel.filter { it is MissionVerificationUiModel.Success }
+        ) { missionBoard, mission, missionVerification ->
+            getMissionState(missionBoard, mission, missionVerification)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(500),
+            initialValue = MissionState.LOADING
+        )
+
+    val boardRewardEvent: SharedFlow<BoardReward?> = missionBoardUiModel.map {
+        when (it) {
+            is MissionBoardUiModel.Success -> {
+                it.missionBoardsResponse.missionBoards.find { result ->
+                    result.isMyPosition && result.number != 0 && result.number != it.missionBoardsResponse.missionBoards.size
+                }?.reward
+            }
+
+            else -> {
+                null
+            }
+        }
+    }.shareIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(500)
+    )
+
     fun getMissionBoards() {
+
         viewModelScope.launch {
             _missionBoardUiModel.emit(MissionBoardUiModel.Loading)
 
@@ -59,14 +100,17 @@ class BoardViewModel @Inject constructor(
                 }.collect {
                     when (it) {
                         is NetworkResult.Success -> {
-                            _missionBoardUiModel.emit(MissionBoardUiModel.Success(it.data))
+                            _missionBoardUiModel.emit(
+                                MissionBoardUiModel.Success(
+                                    it.data
+                                )
+                            )
                         }
 
                         else -> {
                             _missionBoardUiModel.emit(MissionBoardUiModel.Error)
                         }
                     }
-
                 }
         }
     }
@@ -116,5 +160,9 @@ class BoardViewModel @Inject constructor(
                     _deleteMissionResultEvent.emit(true)
                 }
         }
+    }
+
+    fun verify() {
+
     }
 }
