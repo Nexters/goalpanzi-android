ac2d356c6c2d899794d7add3c815c74db7b1ba6d
eshc123 <>
2024-08-16 23:08:45 +0900

    mod MissionBoardsResponse, MissionDetailResponse 수정

--- core/model/src/main/java/com/luckyoct/core/model/response/MissionBoardsResponse.kt
+++ core/model/src/main/java/com/luckyoct/core/model/response/MissionBoardsResponse.kt
@@ -7,4 +7,13 @@ data class MissionBoardsResponse(
     val missionBoards : List<MissionBoardResponse>,
     val progressCount : Int,
     val rank : Int
-)
+){
+    val boardRewardList : List<MissionBoardResponse> by lazy {
+        missionBoards.filter { it.reward != BoardReward.NONE }
+    }
+
+    val passedCountByMe : Int
+        get() {
+            return missionBoards.find { it.isMyPosition }?.number ?: 0
+        }
+}
