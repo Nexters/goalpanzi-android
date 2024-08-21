c1e683fdd639865a7499f67cefd33d1ac138f941
Changyeop Lee <paulus0617@gmail.com>
2024-08-21 21:46:53 +0900

    Update version code from 1 to 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.goalpanzi.mission_mate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
