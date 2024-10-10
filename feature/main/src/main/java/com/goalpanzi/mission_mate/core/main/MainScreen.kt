e0fb3f66d7ed95aa83573c6ec01d4ffa43e359a7
eshc123 <>
2024-10-11 01:16:54 +0900

    feat: Main - 401 시 로그인 화면으로 이동

--- feature/main/src/main/java/com/goalpanzi/mission_mate/core/main/MainScreen.kt
+++ feature/main/src/main/java/com/goalpanzi/mission_mate/core/main/MainScreen.kt
@@ -2,17 +2,25 @@ package com.goalpanzi.mission_mate.core.main
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.goalpanzi.mission_mate.core.main.component.MainNavHost
 import com.goalpanzi.mission_mate.core.main.component.MainNavigator
 import com.goalpanzi.mission_mate.core.main.component.rememberMainNavigator
-import com.goalpanzi.mission_mate.core.navigation.RouteModel
 
 @Composable
 internal fun MainScreen(
+    startDestination: String,
     navigator: MainNavigator = rememberMainNavigator(),
-    startDestination: String
+    viewModel: MainViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(Unit) {
+        viewModel.navigationEvent.collect {
+            navigator.navController.navigate(it)
+        }
+    }
+
     MainScreenContent(
         navigator = navigator,
         startDestination = startDestination
