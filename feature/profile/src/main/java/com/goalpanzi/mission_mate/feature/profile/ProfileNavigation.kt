26dfb1b06f44b408c18575392192fa0d8d93250e
eshc123 <>
2025-01-09 22:31:55 +0900

    refactor: Profile, Setting navigation 마이그레이션

--- feature/profile/src/main/java/com/goalpanzi/mission_mate/feature/profile/ProfileNavigation.kt
+++ feature/profile/src/main/java/com/goalpanzi/mission_mate/feature/profile/ProfileNavigation.kt
@@ -5,24 +5,25 @@ import androidx.compose.animation.core.tween
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.goalpanzi.mission_mate.core.navigation.RouteModel
 
 enum class ProfileSettingType {
     CREATE, SETTING
 }
 
 fun NavController.navigateToProfileCreate() {
-    this.navigate("RouteModel.Profile.Create")
+    this.navigate(RouteModel.Profile.Create)
 }
 
 fun NavController.navigateToProfileSetting() {
-    this.navigate("RouteModel.Profile.Setting")
+    this.navigate(RouteModel.Profile.Setting)
 }
 
 fun NavGraphBuilder.profileNavGraph(
     onSaveSuccess: () -> Unit,
     onBackClick: () -> Unit
 ) {
-    composable("RouteModel.Profile.Create",
+    composable<RouteModel.Profile.Create>(
         enterTransition = {
             slideIntoContainer(
                 towards = AnimatedContentTransitionScope.SlideDirection.Left,
@@ -35,7 +36,7 @@ fun NavGraphBuilder.profileNavGraph(
             onSaveSuccess = onSaveSuccess
         )
     }
-    composable("RouteModel.Profile.Setting") {
+    composable<RouteModel.Profile.Setting> {
         ProfileRoute(
             profileSettingType = ProfileSettingType.SETTING,
             onSaveSuccess = onBackClick,
--- feature/setting/src/main/java/com/goalpanzi/mission_mate/feature/setting/navigation/SettingNavigation.kt
+++ feature/setting/src/main/java/com/goalpanzi/mission_mate/feature/setting/navigation/SettingNavigation.kt
@@ -5,23 +5,21 @@ import androidx.compose.animation.core.tween
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.goalpanzi.mission_mate.core.navigation.RouteModel
+import com.goalpanzi.mission_mate.core.navigation.SettingRouteModel
 import com.goalpanzi.mission_mate.feature.setting.screen.SettingRoute
 import com.goalpanzi.mission_mate.feature.setting.screen.WebViewScreen
 
 fun NavController.navigateToSetting() {
-    this.navigate("RouteModel.Setting")
-}
-
-fun NavController.navigateToInquiry() {
-    this.navigate("SettingRouteModel.Inquiry")
+    this.navigate(RouteModel.Setting)
 }
 
 fun NavController.navigateToServicePolicy() {
-    this.navigate("SettingRouteModel.ServicePolicy")
+    this.navigate(SettingRouteModel.ServicePolicy)
 }
 
 fun NavController.navigateToPrivacyPolicy() {
-    this.navigate("SettingRouteModel.PrivacyPolicy")
+    this.navigate(SettingRouteModel.PrivacyPolicy)
 }
 
 fun NavGraphBuilder.settingNavGraph(
@@ -31,7 +29,7 @@ fun NavGraphBuilder.settingNavGraph(
     onClickPrivacyPolicy: () -> Unit,
     onClickLogout: () -> Unit
 ) {
-    composable("RouteModel.Setting",
+    composable<RouteModel.Setting>(
         enterTransition = {
             slideIntoContainer(
                 towards = AnimatedContentTransitionScope.SlideDirection.Start,
@@ -54,7 +52,7 @@ fun NavGraphBuilder.settingNavGraph(
 fun NavGraphBuilder.servicePolicyNavGraph(
     onBackClick: () -> Unit
 ) {
-    composable("SettingRouteModel.ServicePolicy") {
+    composable<SettingRouteModel.ServicePolicy> {
         WebViewScreen(
             onBackClick = onBackClick,
             url = "https://missionmate.notion.site/f638866edeaf45b58ef63d1000f30c15?pvs=73"
@@ -65,7 +63,7 @@ fun NavGraphBuilder.servicePolicyNavGraph(
 fun NavGraphBuilder.privacyPolicyNavGraph(
     onBackClick: () -> Unit
 ) {
-    composable("SettingRouteModel.PrivacyPolicy") {
+    composable<SettingRouteModel.PrivacyPolicy> {
         WebViewScreen(
             onBackClick = onBackClick,
             url = "https://missionmate.notion.site/c79e9e6990de466490c584f351b364b7?pvs=4"
