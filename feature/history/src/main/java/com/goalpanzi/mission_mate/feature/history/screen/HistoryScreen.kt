30239f4853c2dc060d2a51263128e03b984a98ef
eshc123 <>
2025-02-06 07:54:08 +0900

    feat: 내기록 타이틀 추가

--- feature/history/src/main/java/com/goalpanzi/mission_mate/feature/history/screen/HistoryScreen.kt
+++ feature/history/src/main/java/com/goalpanzi/mission_mate/feature/history/screen/HistoryScreen.kt
@@ -1,12 +1,21 @@
 package com.goalpanzi.mission_mate.feature.history.screen
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.goalpanzi.mission_mate.core.designsystem.theme.ColorGray1_FF404249
 import com.goalpanzi.mission_mate.core.designsystem.theme.MissionMateTypography
+import com.goalpanzi.mission_mate.core.designsystem.theme.MissionmateTheme
+import com.goalpanzi.mission_mate.feature.history.R
 
 @Composable
 fun HistoryRoute(
@@ -21,13 +30,31 @@ fun HistoryRoute(
 fun HistoryScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text(
-            text = "준비중입니다",
-            style = MissionMateTypography.body_md_bold
-        )
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        HistoryTitle()
+    }
+}
+
+@Composable
+fun HistoryTitle(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = R.string.history_title),
+        modifier = modifier
+            .wrapContentWidth()
+            .padding(start = 24.dp, top = 20.dp, bottom = 16.dp, end = 24.dp),
+        style = MissionMateTypography.heading_sm_bold,
+        color = ColorGray1_FF404249
+    )
+}
+
+@Preview
+@Composable
+private fun HistoryScreenPreview() {
+    MissionmateTheme {
+        HistoryScreen()
     }
 }
