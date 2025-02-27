f70ba8ddd0419b5c3f0888e43e045c562890dc5d
eshc123 <>
2025-02-27 21:58:00 +0900

    refactor: 페이지 계산 개선

--- feature/history/src/main/java/com/goalpanzi/mission_mate/feature/history/HistoryViewModel.kt
+++ feature/history/src/main/java/com/goalpanzi/mission_mate/feature/history/HistoryViewModel.kt
@@ -1,6 +1,5 @@
 package com.goalpanzi.mission_mate.feature.history
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.goalpanzi.mission_mate.core.domain.common.DomainResult
@@ -19,34 +18,34 @@ class HistoryViewModel @Inject constructor(
     private val getMissionHistoriesUseCase: GetMissionHistoriesUseCase
 ) : ViewModel() {
 
-    private var _page = 0
-
     private val _historyState = MutableStateFlow<HistoryUiState>(HistoryUiState.Loading)
     val historyState: StateFlow<HistoryUiState> = _historyState.asStateFlow()
 
     fun initHistories() {
         viewModelScope.launch {
             _historyState.emit(HistoryUiState.Loading)
-            getHistories(0)
+            getHistories()
         }
     }
 
     fun refresh() {
         viewModelScope.launch {
             _historyState.emit(HistoryUiState.Refreshing)
-            getHistories(0)
+            getHistories()
         }
     }
 
     fun fetchHistories() {
         viewModelScope.launch {
-            getHistories(_page + 1)
+            getHistories(getNextPage())
         }
     }
 
-    private suspend fun getHistories(page: Int) {
-        _page = page
-        getMissionHistoriesUseCase(page).collect { result ->
+    private suspend fun getHistories(page: Int = 0) {
+        getMissionHistoriesUseCase(
+            page = page,
+            pageSize = HISTORIES_PAGE_SIZE
+        ).collect { result ->
             val uiState = when (result) {
                 is DomainResult.Success -> {
                     val data = result.data.toUiModel()
@@ -74,5 +73,18 @@ class HistoryViewModel @Inject constructor(
     ): List<History> {
         return if (historyUiState is HistoryUiState.Success) historyUiState.histories.resultList
         else emptyList()
+    }
+
+    private fun getNextPage() : Int {
+        return when(val state = historyState.value) {
+            is HistoryUiState.Success -> {
+                (state.histories.resultList.size % HISTORIES_PAGE_SIZE) + 1
+            }
+            else -> 0
+        }
+    }
+
+    companion object {
+        private const val HISTORIES_PAGE_SIZE = 30
     }
 }
